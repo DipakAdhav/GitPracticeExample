3554beb8557f8c8842a97bc66fd1363cd7369b30
Dipak Adhav <dipakadhav798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798798@gmail.com>
2023-06-22 17:07:04 +0530

    Add Addition functionality in substraction class

--- src/example/Substraction.java
+++ src/example/Substraction.java
@@ -1,5 +1,10 @@
 package example;
 
-public class Substraction {
+public class Substraction extends Example {
+	
+	public void Add(int a , int b)
+	{
+		System.out.println("Addition is" + a+b);
+	}
 
 }
